87277af382bda8f48ac487e35816250be41a06fd
zozi0406 <aldott.zozi@gmail.com>
2021-11-28 14:58:52 +0000

    Major changes to queries.

--- AssignmentA1.sql
+++ AssignmentA1.sql
@@ -26,9 +26,10 @@ CREATE TABLE Reservation (
   Smoking_preferred INTEGER,
   Nr_beds_preferred INTEGER,
   High_or_low_floor_preferred TEXT,
-  -- In this case Text is chosen for simplicity. Date operations work on all of them. SQLite (2021): "https://www.sqlite.org/datatype3.html"
-  Arrival_date TEXT NOT NULL,
-  Departure_date TEXT NOT NULL,
+  -- SQLite does not have a Date data type, it can be stored as either Text, Real or Integer
+  -- In this case Integer is chosen for simplicity. Date operations work on all of them. SQLite (2021): "https://www.sqlite.org/datatype3.html"
+  Arrival_date INTEGER NOT NULL,
+  Departure_date INTEGER NOT NULL,
   Credit_card_num INTEGER NOT NULL,
   Credit_card_expiry_year INTEGER NOT NULL,
   credit_card_expiry_month INTEGER NOT NULL,
@@ -49,9 +50,10 @@ CREATE TABLE Additional_services (
 CREATE TABLE Stay (
   Stay_id PRIMARY KEY AUTOINCREMENT,
   Guest_id INTEGER NOT NULL,
-  -- In this case Text is chosen for simplicity. Date operations work on all of them. SQLite (2021): "https://www.sqlite.org/datatype3.html"
-  Arrival_date TEXT NOT NULL,
-  Departure_date TEXT NOT NULL,
+  -- SQLite does not have a Date data type, it can be stored as either Text, Real or Integer
+  -- In this case Integer is chosen for simplicity. Date operations work on all of them. SQLite (2021): "https://www.sqlite.org/datatype3.html"
+  Arrival_date INTEGER NOT NULL,
+  Departure_date INTEGER NOT NULL,
   Channel_id TEXT,
   Channel_fee REAL,
   FOREIGN KEY (Guest_id) REFERENCES Guest(Guest_id),
@@ -123,8 +125,8 @@ CREATE TABLE Room_allocation (
   Hotel_id INTEGER NOT NULL,
   Room_name_or_number INTEGER NOT NULL,
   -- SQLite does not have a Date data type, it can be stored as either Text, Real or Integer
-  -- In this case Text is chosen for simplicity. Date operations work on all of them. SQLite (2021): "https://www.sqlite.org/datatype3.html"
-  Date TEXT NOT NULL,
+  -- In this case Integer is chosen for simplicity. Date operations work on all of them. SQLite (2021): "https://www.sqlite.org/datatype3.html"
+  Date INTEGER NOT NULL,
   Allocation_type TEXT,
   Channel_id INTEGER DEFAULT NULL,
   Reservation_id INTEGER DEFAULT NULL,
@@ -144,60 +146,96 @@ CREATE TABLE Room_allocation (
 -- Queries
 -- 1. The total spent for the customer for a particular stay (checkout invoice).
 
-select G.Guest_id, S.Stay_id,P.Amt from Stay S join Invoice I on S.Stay_id = I.Stay_id 
-join Invoice_payments IP on IP.Invoice_id= I.Invoice_id 
-group by S.Stay_id;
---where S.Stay_id = 12345;
+--Assuming total spent includes taxes paid.
+SELECT S.Guest_id, S.Stay_id, SUM(IP.Amount) FROM Stay S 
+INNER JOIN Invoice I ON S.Stay_id = I.Stay_id 
+INNER JOIN Invoice_payments IP ON IP.Invoice_id = I.Invoice_id 
+-- To specify a particular stay:
+WHERE S.Stay_id = 12345;
 
 
---The most valuable customers in (a) the last two months, (b) past year and (c) from thebeginning of the records.
+
+
+--The most valuable customers in (a) the last two months, (b) past year and (c) from the beginning of the records.
 
 --a
 
-select G.First_name, G.Last_name, sum(IP.Amt) as Amount from Guest G join Stay S on G.Guest_id=S.Guest_id
-join Invoice I I.Stay_id= S.Stay_id join Invoice_payments IP on IP.Invoice_id= I.Invoice_id 
-Where S.Departure_date >= DateADD(M,-2,getdate())
-order by Amount desc Limit 1;
+-- Assuming last two months means to count all stays that started in the last two months.
+
+SELECT G.Guest_id, SUM(IP.Amount) AS Total_spent FROM Stay S 
+INNER JOIN Guest G ON G.Guest_id = S.Guest_id
+INNER JOIN Invoice I ON S.Stay_id = I.Stay_id 
+INNER JOIN Invoice_payments IP ON IP.Invoice_id = I.Invoice_id 
+-- Based on https://www.sqlite.org/lang_datefunc.html
+WHERE date(S.Arrival_date) >= date('now',"-2 months")
+GROUP BY G.Guest_id
+ORDER BY Ex_tax_value DESC LIMIT 10;
 
 --b
-select G.First_name, G.Last_name, sum(IP.Amt) as Amount from Guest G join Stay S on G.Guest_id=S.Guest_id
-join Invoice I I.Stay_id= S.Stay_id join Invoice_payments IP on IP.Invoice_id= I.Invoice_id 
-Where S.Departure_date >= DateADD(M,-12,getdate())
-order by Amount desc Limit 1;
+
+SELECT G.Guest_id, SUM(IP.Amount) AS Total_spent FROM Stay S 
+INNER JOIN Guest G ON G.Guest_id = S.Guest_id
+INNER JOIN Invoice I ON S.Stay_id = I.Stay_id 
+INNER JOIN Invoice_payments IP ON IP.Invoice_id = I.Invoice_id 
+-- Based on https://www.sqlite.org/lang_datefunc.html, Assuming by past year, the last 365 days are meant.
+-- If all stays since the start of the year are meant, "-1 year" would be replaced by "start of year".
+WHERE date(S.Arrival_date) >= date('now',"-1 year")
+GROUP BY G.Guest_id
+ORDER BY Ex_tax_value DESC LIMIT 10;
 
 --c
-select G.First_name, G.Last_name, sum(IP.Amt) as Amount from Guest G join Stay S on G.Guest_id=S.Guest_id
-join Invoice I I.Stay_id= S.Stay_id join Invoice_payments IP on IP.Invoice_id= I.Invoice_id 
-order by Amount desc Limit 1;
+
+SELECT G.Guest_id, SUM(IP.Amount) AS Total_spent FROM Stay S 
+INNER JOIN Guest G ON G.Guest_id = S.Guest_id
+INNER JOIN Invoice I ON S.Stay_id = I.Stay_id 
+INNER JOIN Invoice_payments IP ON IP.Invoice_id = I.Invoice_id 
+GROUP BY G.Guest_id
+ORDER BY Ex_tax_value DESC LIMIT 10;
 
 
 -- Which are the top countries where our customers come from ?
 
-select G.Country, Count(Country) as Frequency from Guest G join Stay S
-on G.Guest_id=S.Guest_id
-group by (S.Guest_id)
-order by Frequency desc limit 5;
+SELECT Country, COUNT(Guest_id) AS Frequency FROM Guest 
+GROUP BY Country
+ORDER BY Frequency DESC LIMIT 10;
 
 
 -- How much did the hotel pay in referral fees for each of the platforms that we have contracted with?
 
-select C.Channel_id,C.Channel_name, sum(c.Channel_fee)
-from S.stay join Channel_name C on S.Channel_id=C.Channel_id
-group by S.Channel_id
+
+SELECT C.Channel_id, C.Channel_name, SUM(S.Channel_fee) AS Total_fees FROM Booking_channel C
+INNER JOIN Stay S on S.Channel_id=C.Channel_id
+-- Only include stays that finished on or before the end of the last month
+WHERE date(S.Departure_date) <= date("now","start of month","-1 day")
+GROUP BY C.Channel_name;
 
 
 --What is the utilization rate for each hotel (that is the average billable days of a hotel specified as the average utilization of room bookings for the last 12 months)
 
-select H.Name , H.Hotel_id, count(distinct(RA.date))/365 as Frequency 
-from  Room_allocation RA  join Hotel H on H.Hotel_id= Ra.Hotel_id
-group by RA.Hotel_id having Ra.date >= DateADD(M,-12,getdate())
+SELECT Name, Hotel_id, AVG(Utilization) FROM
+    (
+    SELECT H.Name, H.Hotel_id, R.Room_name_or_number, COUNT(RA.date)/365.0 AS Utilization FROM 
+    Room_allocation RA  
+    --Only joining to retrieve hotel name:
+    INNER JOIN Room R on RA.(Hotel_id, Room_name_or_number) = R.(Hotel_id, Room_name_or_number)
+    INNER JOIN Hotel H on R.Hotel_id = H.Hotel_id
+    WHERE date(RA.Date) >= date("now","-1 year")
+    --To ensure that only stays count as utilised days:
+    AND Stay_id NOT NULL
+    GROUP BY R.Hotel_id, R.Room_name_or_number
+    )
+GROUP BY Hotel_id;
+
 
 --Calculate the Customer Value in terms of total spent for each customer before the current booking.
 
-select RA.Reservation_id, R.Guest_id, sum(IV.Amt) from Reservation R 
-join Room_allocation RA on R.Reservation_id=Reservation_id 
-join Stay S on  S.Guest_id=RA.Guest_id 
-join Invoice on I I.Stay_id=S>Stay_id 
-join Invoice_payments IP on IP.Invoice_id = I.Invoice_id
-group by RA.Guest_id
+--Assuming total spent includes taxes paid
+--Stay only includes previous stays, so no need to filter anything.
+SELECT S.Guest_id, SUM(IP.Amount) AS Total_spent FROM Stay S
+INNER JOIN Invoice I ON I.Stay_id=S.Stay_id 
+INNER JOIN Invoice_payments IP ON IP.Invoice_id = I.Invoice_id
+--Guest_id could be specified if looking for specific
+--WHERE S.Guest_id = 12345
+GROUP BY S.Guest_id
+
 
